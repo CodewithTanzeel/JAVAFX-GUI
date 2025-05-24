5f27f6fa6bd94d362f2e2476063cd8e4efb72965
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-05-25 01:30:18 +0500

    .

---
+++ ListViewJavaFx/src/listviewjavafx/ListViewJavaFx.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listviewjavafx;
+
+/**
+ *
+ * @author FATTANI COMPUTERS
+ */
+public class ListViewJavaFx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
